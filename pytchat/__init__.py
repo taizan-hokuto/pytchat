ca266ef04b0fc28a729fa4c2b7f643658c0814e8
55448286+taizan-hokuto@users.noreply.github.com <anon@anon.c>
2019-11-03 14:12:24 +0900

    Increment version

--- pytchat/__init__.py
+++ pytchat/__init__.py
@@ -2,11 +2,11 @@
 pytchat is a python library for fetching youtube live chat.
 """
 __copyright__    = 'Copyright (C) 2019 taizan-hokuto'
-__version__      = '0.0.1.7'
+__version__      = '0.0.1.8'
 __license__      = 'MIT'
 __author__       = 'taizan-hokuto'
 __author_email__ = '55448286+taizan-hokuto@users.noreply.github.com'
-__url__          = 'https://github.com/taizan-hokuto'
+__url__          = 'https://github.com/taizan-hokuto/pytchat'
 
 __all__ = ["core_async","core_multithread","processors"]
 
