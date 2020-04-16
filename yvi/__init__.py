3aee1b276f9bfc41cc6ba90c58c05555f614db34
taizan-hokuto <55448286+taizan-hokuto@users.noreply.github.com>
2020-04-17 00:23:27 +0900

    Increment version

--- yvi/__init__.py
+++ yvi/__init__.py
@@ -2,7 +2,7 @@
 Retriever tool for youtube video information.
 """
 __copyright__    = 'Copyright (C) 2020 taizan-hokuto'
-__version__      = '0.0.1'
+__version__      = '0.0.2'
 __license__      = 'MIT'
 __author__       = 'taizan-hokuto'
 __author_email__ = '55448286+taizan-hokuto@users.noreply.github.com'
