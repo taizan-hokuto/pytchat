7500f79de0930069ab079169e556984f684f82b9
taizan-hokuto <55448286+taizan-hokuto@users.noreply.github.com>
2020-06-18 00:16:57 +0900

    Increment version

--- pytchat/config/__init__.py
+++ pytchat/config/__init__.py
@@ -1,7 +1,7 @@
 from . import mylogger
 
 headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'}
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36'}
 
 
 def logger(module_name: str, loglevel=None):
