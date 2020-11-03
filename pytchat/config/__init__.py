fcddc1516b7ca42ba367c07af18a3ff0d477cbce
taizan-hokouto <55448286+taizan-hokuto@users.noreply.github.com>
2020-11-03 18:19:43 +0900

    Increment version

--- pytchat/config/__init__.py
+++ pytchat/config/__init__.py
@@ -1,10 +1,10 @@
-import logging
+import logging  # noqa
 from . import mylogger
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36',
 }
 
 
-def logger(module_name: str, loglevel=logging.DEBUG):
+def logger(module_name: str, loglevel=None):
     module_logger = mylogger.get_logger(module_name, loglevel=loglevel)
     return module_logger
