acfba7482112a0042673630aa3082434affa1e71
taizan-hokouto <55448286+taizan-hokuto@users.noreply.github.com>
2021-02-11 01:56:43 +0900

    Delete tests

--- tests/test_liveparam.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from pytchat.paramgen import liveparam
-
-def test_liveparam_0(mocker):
-    _ts1= 1546268400
-    param = liveparam._build("01234567890",
-    *([_ts1*1000000 for i in range(5)]), topchat_only=False)
-    test_param="0ofMyAN1GhxDZzhLRFFvTE1ERXlNelExTmpjNE9UQWdBUT09KIC41tWqyt8CMAA4AEABShsIABAAGAAgADoAQABKAFCAuNbVqsrfAlgDeABQgLjW1arK3wJYgLjW1arK3wJoAYIBAggBiAEAmgECCACgAYC41tWqyt8C"
-    assert test_param == param
